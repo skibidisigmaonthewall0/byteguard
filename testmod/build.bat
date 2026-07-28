5d2943bbd4d242b1b7795092a3df3cc3e7be90c2
MarkDev1337 <markdev1337@github.com>
2026-07-28 21:32:06 +0300

    Remove test mod and test folder

--- testmod/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Building ObfuscationTest Mod with Java 21...
-set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
-"%JAVA_EXE%" -classpath gradle/wrapper/gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain build %*
